1de4c7dc67a814dfc7d6ffd67c9a25e821d215ed
LordofMankid <58374778+LordofMankid@users.noreply.github.com>
2025-06-24 12:13:16 -0400

    added initial navbar styling

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import Logo from "../assets/freebites.svg";
+import CommonButton from "./common/CommonButton";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div>
-      <Logo className="w-16 h-20" />
+    <div className="flex flex-row h-32 items-center justify-between mx-20">
+      <Logo className="w-14 h-16" />
+      <div className="flex flex-row h-11 items-center gap-12">
+        <Link href="/mission">Mission</Link>
+        <Link href="/team">Team</Link>
+        <Link href="/contact">Contact</Link>
+        <div className="flex flex-row h-11 items-center gap-4">
+          <CommonButton label={"sign up"} />
+          <CommonButton label={"consult with freebites"} />
+        </div>
+      </div>
     </div>
   );
 };
---
+++ app/components/common/CommonButton.tsx
@@ -0,0 +1,56 @@
+"use client";
+import React from "react";
+
+interface ButtonProps {
+  label: string;
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  leftIcon?: React.ReactElement;
+  rightIcon?: React.ReactElement;
+  altStyle?: string;
+  altTextStyle?: string;
+  disabled?: boolean;
+  type?: HTMLButtonElement["type"];
+  noFocusStyle?: boolean;
+}
+
+/**
+ * Our internal button component which includes options for icons and adheres to our own styling system.
+ * @param props: label
+ * @param optional props: left/right icons, alternate Tailwind classes for the text and the
+ * @returns a React Components.
+ * @TODO: add in loading components
+ */
+
+const CommonButton = (props: ButtonProps) => {
+  const {
+    label,
+    onClick,
+    leftIcon,
+    rightIcon,
+    altStyle,
+    altTextStyle,
+    disabled,
+    type,
+  } = props;
+
+  // if icon isn't passed in, nothing will show
+  return (
+    <button
+      onClick={onClick}
+      className={`flex flex-row items-center justify-center gap-2 py-2.5 px-5 min-w-max border rounded-[100px] border-black bg-white" ${altStyle}`}
+      disabled={disabled}
+      type={type ?? "button"}
+    >
+      {leftIcon ? leftIcon : null}
+      <p
+        className={`text-dark-blue text-sm font-medium font-[family-name:var(--font-rubik)] ${altTextStyle}`}
+      >
+        {label}
+      </p>
+
+      {rightIcon ? rightIcon : null}
+    </button>
+  );
+};
+
+export default CommonButton;
