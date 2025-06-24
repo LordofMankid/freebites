b8f9c66e42efb5817cbb7b845ddbe2fe49506e14
Owen Prendergast <owenprendergast8@gmail.com>
2025-06-24 15:11:52 -0700

    clean freebizzy

--- app/components/Freebizzy.tsx
+++ app/components/Freebizzy.tsx
@@ -1,40 +1,40 @@
-import React from "react";
-import { useRef, useEffect } from "react";
-import { animate, createScope, createSpring, Scope } from "animejs";
-import Logo from "../assets/freebites.svg";
+// import React from "react";
+// import { useRef, useEffect } from "react";
+// import { animate, createScope, createSpring, Scope } from "animejs";
+// import Logo from "../assets/freebites.svg";
 
-const Freebizzy = () => {
-  const root = useRef(null);
-  const scope = useRef<Scope | null>(null);
-  useEffect(() => {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    // test again again
-    scope.current = createScope({ root }).add((self) => {
-      animate("#logoTitle", {
-        scale: [
-          { to: 2, ease: "inOut(3)", duration: 200 },
-          { to: 1, ease: createSpring({ stiffness: 300 }) },
-        ],
+// const Freebizzy = () => {
+//   const root = useRef(null);
+//   const scope = useRef<Scope | null>(null);
+//   useEffect(() => {
+//     // eslint-disable-next-line @typescript-eslint/no-unused-vars
+//     // test again again
+//     scope.current = createScope({ root }).add((self) => {
+//       animate("#logoTitle", {
+//         scale: [
+//           { to: 2, ease: "inOut(3)", duration: 200 },
+//           { to: 1, ease: createSpring({ stiffness: 300 }) },
+//         ],
 
-        loop: true,
-        loopDelay: 250,
-      });
-    });
-    return () => {
-      if (scope.current) scope.current.revert();
-    };
-  }, []);
-  return (
-    <div
-      ref={root}
-      className="w-screen h-screen flex flex-row justify-center items-center"
-    >
-      <div id="logoTitle" className="flex flex-col justify-center items-center">
-        <Logo />
-        <p className="text-4xl font-semibold">Freebizzy</p>
-      </div>
-    </div>
-  );
-};
+//         loop: true,
+//         loopDelay: 250,
+//       });
+//     });
+//     return () => {
+//       if (scope.current) scope.current.revert();
+//     };
+//   }, []);
+//   return (
+//     <div
+//       ref={root}
+//       className="w-screen h-screen flex flex-row justify-center items-center"
+//     >
+//       <div id="logoTitle" className="flex flex-col justify-center items-center">
+//         <Logo />
+//         <p className="text-4xl font-semibold">Freebizzy</p>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default Freebizzy;
+// export default Freebizzy;
