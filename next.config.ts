4c4eb4857ae6d54f6d4d2d39f81306dffb9174a1
LordofMankid <58374778+LordofMankid@users.noreply.github.com>
2025-06-24 12:34:33 -0400

    added webpack for svgr build

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,14 @@ const nextConfig: NextConfig = {
       },
     },
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: { and: [/\.(js|ts|jsx|tsx)$/] }, // Ensure this rule only applies to imports within JS/TS files
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
