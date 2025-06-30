955069248dcd8d83f3b84ac1879f4aacdc9d084c
LordofMankid <58374778+LordofMankid@users.noreply.github.com>
2025-06-29 22:34:04 -0400

    added scrolling timeline

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,14 @@ import BottomHero from "./components/BottomHero";
 import ImageTextBlock from "./components/ImageTextBlock";
 import tempImg from "./assets/tempImg.png";
 import { useEffect, useRef } from "react";
-import { createScope, createTimeline, Scope, stagger } from "animejs";
+import {
+  animate,
+  createScope,
+  createTimeline,
+  onScroll,
+  Scope,
+  stagger,
+} from "animejs";
 
 export default function Home() {
   const root = useRef(null);
@@ -30,23 +37,26 @@ export default function Home() {
     ].filter(Boolean); // Filter out null refs
 
     scope.current = createScope({ root }).add(() => {
-      const tl = createTimeline();
-
-      tl.add(
-        targets,
-        {
+      targets.forEach((target) => {
+        if (!target) return;
+        animate(target, {
           opacity: [0, 1],
-          delay: stagger(250),
           duration: 1000,
           ease: "out(3)",
-        },
-        400
-      );
+          autoplay: onScroll({
+            target: target,
+            container: ".page",
+            enter: "bottom-=40% top",
+            leave: "top+=40% bottom",
+            sync: "play",
+          }),
+        });
+      });
     });
   }, []);
 
   return (
-    <div className="flex flex-col">
+    <div ref={root} className="page flex flex-col">
       <Navbar />
       <div className="flex flex-col items-center mx-4 lg:mx-24 md:mx-4 gap-20 mb-24">
         <TopHero ref={topHeroRef} altContainerStyle="opacity-0" />
@@ -82,7 +92,7 @@ export default function Home() {
             />
           </div>
         </div>
-        <BottomHero ref={bottomHeroRef} />
+        <BottomHero ref={bottomHeroRef} altContainerStyle="opacity-0" />
       </div>
     </div>
   );
