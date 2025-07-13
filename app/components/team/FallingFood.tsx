4168b7c83c1132152e76bbc7ca37b4bb2406e1fc
Owen Prendergast <owenprendergast8@gmail.com>
2025-07-13 14:44:42 -0700

    manual hitboxes

--- app/components/team/FallingFood.tsx
+++ app/components/team/FallingFood.tsx
@@ -1,7 +1,119 @@
 import { useApplication } from "@pixi/react";
 import { Assets, Container, Sprite, Texture } from "pixi.js";
 import React, { useEffect, useState } from "react";
-import Matter, { Mouse, MouseConstraint } from "matter-js";
+import Matter, { Mouse, MouseConstraint, Vector } from "matter-js";
+
+const vertexMap = new Map<string, Vector[]>([
+  [
+    "cake",
+    [
+      { x: 27, y: 51 },
+      { x: 80, y: 15 },
+      { x: 125, y: 55 },
+      { x: 102, y: 113 },
+      { x: 12, y: 103 },
+    ],
+  ],
+  [
+    "chicken",
+    [
+      { x: 30, y: 30 },
+      { x: 57, y: 17 },
+      { x: 120, y: 41 },
+      { x: 105, y: 81 },
+      { x: 99, y: 83 },
+      { x: 72, y: 122 },
+      { x: 50, y: 121 },
+      { x: 25, y: 105 },
+      { x: 18, y: 94 },
+      { x: 30, y: 50 },
+    ],
+  ],
+  [
+    "fries",
+    [
+      { x: 6, y: 15 },
+      { x: 20, y: 4 },
+      { x: 60, y: 7 },
+      { x: 93, y: 19 },
+      { x: 82, y: 48 },
+      { x: 90, y: 55 },
+      { x: 83, y: 98 },
+      { x: 68, y: 111 },
+      { x: 15, y: 102 },
+    ],
+  ],
+  [
+    "hamburger",
+    [
+      { x: 24, y: 64 },
+      { x: 41, y: 34 },
+      { x: 75, y: 24 },
+      { x: 96, y: 37 },
+      { x: 113, y: 77 },
+      { x: 110, y: 90 },
+      { x: 84, y: 108 },
+      { x: 58, y: 114 },
+      { x: 45, y: 110 },
+    ],
+  ],
+  [
+    "hotdog",
+    [
+      { x: 77, y: 17 },
+      { x: 91, y: 1 },
+      { x: 104, y: 93 },
+      { x: 112, y: 15 },
+      { x: 112, y: 39 },
+      { x: 61, y: 111 },
+      { x: 32, y: 115 },
+      { x: 21, y: 103 },
+      { x: 22, y: 78 },
+      { x: 71, y: 13 },
+    ],
+  ],
+  [
+    "pancake",
+    [
+      { x: 31, y: 71 },
+      { x: 50, y: 44 },
+      { x: 90, y: 28 },
+      { x: 117, y: 34 },
+      { x: 126, y: 50 },
+      { x: 147, y: 59 },
+      { x: 147, y: 76 },
+      { x: 113, y: 108 },
+      { x: 60, y: 126 },
+      { x: 32, y: 121 },
+      { x: 25, y: 111 },
+      { x: 36, y: 87 },
+    ],
+  ],
+  [
+    "pizza",
+    [
+      { x: 22, y: 7 },
+      { x: 69, y: 17 },
+      { x: 95, y: 44 },
+      { x: 87, y: 56 },
+      { x: 76, y: 57 },
+      { x: 16, y: 84 },
+      { x: 10, y: 77 },
+      { x: 21, y: 23 },
+    ],
+  ],
+  [
+    "sandwich",
+    [
+      { x: 11, y: 18 },
+      { x: 90, y: 12 },
+      { x: 105, y: 44 },
+      { x: 111, y: 77 },
+      { x: 37, y: 91 },
+      { x: 4, y: 55 },
+    ],
+  ],
+]);
 
 const FallingFood = () => {
   const { app } = useApplication();
@@ -35,15 +147,25 @@ const FallingFood = () => {
       const sprite = new Sprite(texture);
       sprite.x = x;
       sprite.y = y;
-      sprite.scale = 0.75;
+      sprite.scale = 1;
       sprite.anchor.set(0.5);
       container.addChild(sprite);
 
-      const body = Matter.Bodies.circle(x, y, sprite.height / 2.2, {
+      let body = Matter.Bodies.circle(x, y, sprite.height / 2.2, {
         restitution: 0.75,
         friction: 0.5,
         mass: 1,
       });
+      body = Matter.Bodies.fromVertices(
+        x,
+        y,
+        [vertexMap.get(randFood) ?? [{ x: 0, y: 0 }]],
+        {
+          restitution: 0.75,
+          friction: 0.5,
+          mass: 1,
+        }
+      );
 
       Matter.World.add(world, body);
 
