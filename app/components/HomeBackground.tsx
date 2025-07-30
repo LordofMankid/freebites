6de04f4da12a6922789bc6c79288f24f5106544d
Owen Prendergast <owenprendergast8@gmail.com>
2025-07-30 15:06:04 -0700

    touch fix 3

--- app/components/HomeBackground.tsx
+++ app/components/HomeBackground.tsx
@@ -1,9 +1,15 @@
 import { Application } from "@pixi/react";
-import React, { forwardRef, useRef } from "react";
+import React, { forwardRef, useEffect, useRef, useState } from "react";
 import FallingFood from "./team/FallingFood";
 
 const HomeBackground = forwardRef<HTMLDivElement, object>((props, ref) => {
   const pixiCanvas = useRef(null);
+  const [isTouch, setTouch] = useState<boolean>(false);
+  useEffect(() => {
+    const isTouchDevice =
+      "ontouchstart" in window || navigator.maxTouchPoints > 0;
+    setTouch(isTouchDevice);
+  }, []);
 
   return (
     <div
@@ -15,7 +21,7 @@ const HomeBackground = forwardRef<HTMLDivElement, object>((props, ref) => {
       }}
     >
       <div
-        className="absolute w-full h-screen pointer-events-auto"
+        className={`absolute w-full h-screen pointer-events-${isTouch ? "none" : "auto"}`}
         ref={pixiCanvas}
       >
         <Application
