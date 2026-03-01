4f66825845e74d5891d34ec99d396659c98b403b
LordofMankid <58374778+LordofMankid@users.noreply.github.com>
2026-02-28 19:40:24 -0500

    added privacy and guidelines stuff

---
+++ app/components/mdx-layout.tsx
@@ -0,0 +1,19 @@
+export default function MdxLayout({ children }: { children: React.ReactNode }) {
+  // Create any shared layout or styles here
+  return (
+    <div
+      className="prose max-w-3xl
+      prose-headings:mt-8 prose-headings:font-semibold prose-headings:text-black 
+      prose-h1:text-5xl 
+      prose-h2:text-4xl 
+      prose-h3:text-3xl 
+      prose-h4:text-2xl 
+      prose-h5:text-xl 
+      prose-h6:text-lg 
+      dark:prose-headings:text-white mx-auto
+      pb-20"
+    >
+      {children}
+    </div>
+  );
+}
--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
+import createMDX from "@next/mdx";
 
+const withMDX = createMDX({
+  extension: /\.(md|mdx)$/,
+});
 const nextConfig: NextConfig = {
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   turbopack: {
     rules: {
       "*.svg": {
@@ -27,4 +32,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withMDX(nextConfig);
